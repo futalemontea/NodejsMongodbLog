6780321021bcb7fb732def016df320ff61b422b4
XuZhan <1092794155@qq.com>
2021-12-08 17:18:14 -0800

    change some mistakes

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -3,7 +3,7 @@ const app=express()
 app.use("/",express.static('public'))
 app.get("/input",(req,res)=>{
     res.send(req.query)
-    console.log(req,query)
+    console.log(req.query)
 })
 //123
 app.listen(10131)
